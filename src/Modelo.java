471ce946ec943c04daac7bc28a522b90983d85ac
ikaslea <ikaslea@1AW37>
2017-03-16 12:58:14 +0100

    primer comit

--- src/Modelo.java
+++ src/Modelo.java
@@ -1,4 +1,46 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
-public class Modelo {
+public class Modelo extends Conector {
+
+	Modelo(String zoo) {
+		super(zoo);
+		// TODO Auto-generated constructor stub
+	}
+	public ArrayList select() {
+		
+		ArrayList<Animal> arrayList = new ArrayList();
+		Statement statement;
+		try {
+			statement = super.getConexion().createStatement();
+			ResultSet resultSet = statement.executeQuery("select * from Animales");
+			while (resultSet.next()) {
+				arrayList.add(new Animal(resultSet.getInt("id"), resultSet.getString("nombre")));
+
+			}
+			return arrayList;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	public void insert(Animal animal) {
+		Statement statement;
+		try {
+			statement = super.getConexion().createStatement();
+			statement.execute("INSERT INTO animales (id,nombre) " + "VALUES ('"  + " "
+					+ animal.getId() + " " + animal.getNombre() + "')");
+
+		} catch (SQLException exception) {
+			// TODO Auto-generated catch block
+			exception.printStackTrace();
+		}
+
+	}
+	
+
 
 }
--- src/Principal.java
+++ src/Principal.java
@@ -8,6 +8,9 @@ import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class Principal extends JFrame {
 
@@ -38,19 +41,41 @@ public class Principal extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
 		textField = new JTextField();
-		contentPane.add(textField, BorderLayout.NORTH);
 		textField.setColumns(10);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		contentPane.add(lblNewLabel, BorderLayout.CENTER);
+		JButton btnNewButton_Mostrar = new JButton("Mostrar");
 		
-		JButton btnNewButton = new JButton("New button");
-		contentPane.add(btnNewButton, BorderLayout.SOUTH);
+		JButton btnNewButton_Guardar = new JButton("Guardar");
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, 424, GroupLayout.PREFERRED_SIZE)
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+							.addComponent(btnNewButton_Guardar, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addComponent(btnNewButton_Mostrar, GroupLayout.DEFAULT_SIZE, 424, Short.MAX_VALUE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(181)
+							.addComponent(lblNewLabel)))
+					.addContainerGap(434, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(72)
+					.addComponent(lblNewLabel)
+					.addGap(99)
+					.addComponent(btnNewButton_Guardar)
+					.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addComponent(btnNewButton_Mostrar))
+		);
+		contentPane.setLayout(gl_contentPane);
 	}
-
 }
